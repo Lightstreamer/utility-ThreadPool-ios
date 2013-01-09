8cc99a2afa35f3dded0c985a5dc8f30f3950838c
Gianluca Bertani <gianluca@flyingdolphinstudio.com>
2013-01-09 10:29:42 +0100

    Initial Commit

---
+++ Lightstreamer Thread Pool Library/Lightstreamer_Thread_Pool_Library.h
@@ -0,0 +1,13 @@
+//
+//  Lightstreamer_Thread_Pool_Library.h
+//  Lightstreamer Thread Pool Library
+//
+//  Created by Gianluca Bertani on 09/01/13.
+//  Copyright (c) 2013 Weswit srl. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Lightstreamer_Thread_Pool_Library : NSObject
+
+@end
---
+++ Lightstreamer Thread Pool LibraryTests/Lightstreamer_Thread_Pool_LibraryTests.h
@@ -0,0 +1,13 @@
+//
+//  Lightstreamer_Thread_Pool_LibraryTests.h
+//  Lightstreamer Thread Pool LibraryTests
+//
+//  Created by Gianluca Bertani on 09/01/13.
+//  Copyright (c) 2013 Weswit srl. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Lightstreamer_Thread_Pool_LibraryTests : SenTestCase
+
+@end
