0453fe1972f66ab298c7dd8a63bd2d546618482f
dario-weswit <dario.crivelli@lightstreamer.com>
2015-10-26 17:08:23 +0100

    Standardize copyright notes.

--- Lightstreamer Thread Pool Library/LSLogDelegate.h
+++ Lightstreamer Thread Pool Library/LSLogDelegate.h
@@ -3,7 +3,8 @@
 //  Lightstreamer Thread Pool Library
 //
 //  Created by Gianluca Bertani on 04/02/15.
-//  Copyright (c) Lightstreamer Srl. All rights reserved.
+//
+//  Copyright (c) Lightstreamer Srl
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
